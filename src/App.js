dd4b5063f26baa874b38f25f85dc9350444169a7
Souravdhar7595 <148620289+Souravdhar7595@users.noreply.github.com>
2024-01-14 10:48:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
 function App() {
+const [Color, setColor] = useState("white")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <div className="w-full h-screen " style={{backgroundColor : Color}}>
+<div className="flex item-center justify-between bg-zinc-900">
+  <button className="w-12 h-8 bg-red-600"  onClick={()=>setColor("red")}> red</button>
+  <button className="w-12 h-8 bg-sky-600"  onClick={()=>setColor("blue")}> blue</button>
+  <button className="w-12 h-8 bg-green-600"  onClick={()=>setColor("green")}> green</button>
+  <button className="w-12 h-8 bg-yellow-600"  onClick={()=>setColor("yellow")}> yellow</button>
+  <button className="w-12 h-8 bg-pink-600"  onClick={()=>setColor("pink")}> pink</button>
+</div>
+  </div>
+    </>
   );
 }
 
